e734410af49fc2969eb2c16f9f991a4e6c809ec5
Blobdy <rm94887@estudante.fieb.edu.br>
2025-04-03 08:25:38 -0300

    Envio de parametros

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ function App(){
 
   const [nome,setNome] = useState('Ronaldo')
 
-  const Formulario = () => {
+  const Formulario = (props) => {
     return (
       <div>
-           <input
+        <input
+      placeholder= {props.sombra}
       className="nome"
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
       type="text" />
       <button
-      onClick={()=>{alert(nome)}}
+      onClick={()=>{(props.mensagem) ? alert(props.mensagem) : alert("TEXTO PADRÃO") }}
       className="botao">
-      CLIQUE AQUI 
+      { (props.textobotao !=null) ? props.textobotao : "CLIQUE AQUI"}
       </button>
       </div>
     )
@@ -25,9 +26,10 @@ function App(){
 
     <div>
       <h3>pizzaria 2c</h3>
-      <Formulario/>
-      <Formulario/>
-      <Formulario/>
+      <Formulario sombra = "Digite seu nome" textobotao = "ENVIAR NOME" />
+      <Formulario sombra = "Digite seu email" textobotao = "ENVIAR EMAIL"/>
+      <Formulario sombra = "(11) 9000-0000" textobotao = "ENVIAR NÚMERO"/>
+      <Formulario mensagem = "Mensagem enviada pelo app."/>
 
 
     
